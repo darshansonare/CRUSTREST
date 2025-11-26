c6fc54d755fc365e2852d13b5f46d5a3290b1207
Darshan Sonare <104987000+darshansonare@users.noreply.github.com>
2025-11-26 11:22:47 +0530

    Update Student.java

--- src/main/java/com/example/crudrest/model/Student.java
+++ src/main/java/com/example/crudrest/model/Student.java
@@ -16,14 +16,6 @@ public class Student {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
